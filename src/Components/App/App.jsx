2d447a9ece85da22517d05625e1dd9b29103e75a
Viktor Kindrat <kindratvictor07@gmail.com>
2023-05-29 12:59:01 +0300

    inital

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,23 +1,9 @@
 import './App.css';
 
-import { useSelector, useDispatch } from 'react-redux'
-
 function App() {
-  const data = useSelector(state => state.counter);
-  const dispatch = useDispatch()
-
-  console.log(data)
-  let increment = ()=>{
-    dispatch({type: "counter/increment"})
-  }
-  let decrement = ()=>{
-    dispatch({type: "counter/decrement"})
-  }
   return (
     <div className="App">
-      <h1>Counter has value {data}</h1>
-      <button onClick={increment}>Plus</button>
-      <button onClick={decrement}>Minus</button>
+      <h1>ToDo list</h1>
     </div>
   );
 }
